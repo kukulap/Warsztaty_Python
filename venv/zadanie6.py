297e0e5f9b6d082f97f269df6d15a92e7d02deac
Patrycja <kapron.patrycja@gmail.com>
2018-11-20 10:38:54 +0100

    zad. 6 zmienione - zadanie 3 ze spotkania 2

--- venv/zadanie6.py
+++ venv/zadanie6.py
@@ -1,10 +1,13 @@
 def suma_dzielnikow(n):
     suma = 0
+    lista = []
     i = 1
     while i <= (n-1):
         if n % i == 0:
             suma = suma + i
+            lista.append(i)
         i = i+1
     print("Suma dzielnikow liczby", n, "wynosi:", suma)
+    print(lista)
 
-suma_dzielnikow(6)
+suma_dzielnikow(10)
